d3a0c5412ed05a9d32d3b05fe0d4e3d7375d25e7
Bladee <Leonala@ssfs.org>
2023-11-02 11:52:33 -0400

    omg no way look im so good

--- multiplication.py
+++ multiplication.py
@@ -4,4 +4,16 @@ def multiplication_table(number):
     :param number: An integer
     :return: A string of 12 values representing the mulitiplication table of the parameter number.
     """
-    pass # Make sure to delete this line when writing your function.
+    table = [(str(number * x)) for x in range(1, 13)]
+    return table
+def main():
+    try:
+
+        the_opps = int(input("Select a number to create a multiplication table for: "))
+        if the_opps > 0:
+            print(multiplication_table(the_opps))
+    except ValueError:
+            print("Error, please enter a positive integer.")
+            main()
+if __name__ == "__main__":
+    main()
