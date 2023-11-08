7464ba21ca767dd0e2a9496f6b854dccf0368a28
Bladee <Leonala@ssfs.org>
2023-11-08 08:20:46 -0500

    final code for unit 5 project

--- assignment_five.py
+++ assignment_five.py
@@ -1,7 +1,7 @@
 import random
-def get_number():
+def get_number(): # generates a random number from 1 - 100 that the user must try to guess
     return random.randint(1, 100)
-def get_guess():
+def get_guess(): # obtains a guess from the user
     while True:
         try:
             guess = int(input("Guess a number between 1 and 100: "))
@@ -12,8 +12,9 @@ def get_guess():
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
-def main():
+def main(): # main command that runs the whole program
     print("Welcome to the Number Guessing Game! We will play three games together.")
+    # introductory statement and instructions on what the program will entail
 
     total_guesses = 0
 
@@ -24,9 +25,12 @@ def main():
         guesses = 0
 
         while True:
+            # number guess loop that gets the user closer to the target number until they guess it
             player_guess = get_guess()
             guesses += 1
-
+            '''
+            informational statements that advise the user if they are below or above the target number
+            '''
             if player_guess < target_number:
                 print("Too low. Try a higher number.")
             elif player_guess > target_number:
@@ -38,5 +42,5 @@ def main():
 
     average_guesses = total_guesses / 3
     print("Average number of guesses over 3 games: ", average_guesses)
-
+    '''provides the user the average number of guesses used to reach the target number'''
 main()
